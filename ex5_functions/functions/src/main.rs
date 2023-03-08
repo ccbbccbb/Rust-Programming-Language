5f790329fe91abc4588cbc8268205ab2cada5387
Charles Bourassa <59705750+ccbbccbb@users.noreply.github.com>
2023-03-07 20:13:58 -0500

    005

--- ex5_functions/functions/src/main.rs
+++ ex5_functions/functions/src/main.rs
@@ -1,7 +1,24 @@
-fn main() {
-    another_function(5);
+// fn main() {
+//     another_function(5, 6);
+// }
+
+// fn another_function(x: i32, y: i32) {
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+// }
+
+// regarding expressions, they do not include semi-colons
+// let y = {
+//      let x = 3;
+//      x + 1
+// };
+
+fn five() -> i32 {
+    5
 }
 
-fn another_function(x: i32) {
+fn main() {
+    let x = five();
+
     println!("The value of x is: {}", x);
 }
